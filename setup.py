ff389cde958bea0666ec1d5b19aaa2bac01a5a04
patlewig <patlewig@hotmail.com>
2024-03-04 20:40:57 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Code repository supporting manuscript entitled MetSim: Integrated Programmatic Access and Pathway Management for Xenobiotic Metabolism Simulators',
+    author='patlewig',
+    license='MIT',
+)
